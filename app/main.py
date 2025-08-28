6b0ee82b7150a0bd8ed9b1a71a9ad71844b558b0
srisailamkakurala <srisailamkakurala@gmail.com>
2025-08-28 22:57:23 +0530

    port cahnged from 8000 -> 8001

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn # type: ignore
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
